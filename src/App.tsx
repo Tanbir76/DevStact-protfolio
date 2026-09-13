19299dea61bacbe0843cd497554b293f4dac688d
Tanbir Hasan <tanbirhasan7619@gmail.com>
2026-09-13 14:09:33 +0600

    falback  loding...style it

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,23 @@ import type { CatagoryType } from './assets/type/CatagoryType'
 import Footer from './assets/component/Footer'
 import FooterCopy from './assets/component/FooterCopy'
 
-const asyncPromise = async():Promise<CatagoryType[]> =>{
+const asyncPromise = async (): Promise<CatagoryType[]> => {
   const res = await fetch('/data.json');
   const data = await res.json();
   return data;
 }
 
 function App() {
-     const exploreData = asyncPromise();
+  const exploreData = asyncPromise();
 
   return (
     <>
       <div>
         <Nav />
         <Banner />
-        <Suspense fallback={<h1>Loding....</h1>}>
-        <ExploreTec exploreData ={exploreData} />
+        <Suspense fallback={<h1 className='container mx-auto  text-center text-xl text-[#DB2777]'> <span className="loading loading-spinner text-error"></span>
+          Loding<span className="loading loading-dots loading-xs"></span></h1>}>
+          <ExploreTec exploreData={exploreData} />
         </Suspense>
         <Footer />
         <FooterCopy />
