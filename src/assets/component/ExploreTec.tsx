7d7942be581427a7ee85bb440bfa5a948bacb317
Tanbir Hasan <tanbirhasan7619@gmail.com>
2026-09-13 10:36:24 +0600

    stack card button problem fixed

--- src/assets/component/ExploreTec.tsx
+++ src/assets/component/ExploreTec.tsx
@@ -15,7 +15,7 @@ function ExploreTec({ exploreData }: ExploreData) {
         <div className="container mx-auto space-y-1.5">
             <h1 className="font-black text-3xl bg-linear-to-r from-[#ff5622dc] via-[#db177cd8] to-[#5b06ee] bg-clip-text text-transparent  ">Explore the Technologies</h1>
             <p className="text-[18px] text-[#0f172a9d]  ">Pick one technology per category to build your ideal stack.</p>
-            <div className="grid md:grid-cols-4 gap-4 mt-10">
+            <div className="md:grid md:grid-cols-4 gap-4 mt-10">
                 <div className="col-span-3">
                     <div className="grid md:grid-cols-3 gap-10 ">
                         {
@@ -35,7 +35,7 @@ function ExploreTec({ exploreData }: ExploreData) {
                 </div>
 
                 {/* Slected-Stack-Card */}
-                <div>
+                <div className="mt-10">
                     <div className=" rounded-2xl p-3 space-y-1.5 shadow">
                         <h1 className="font-bold text-[18px]">Youre Stack</h1>
                         <h4 className="text-[16px] text-[#94A3B8]"> {count > 0 ? `${count} Technology Selected` : "No Technology Selected"} </h4>
@@ -43,12 +43,12 @@ function ExploreTec({ exploreData }: ExploreData) {
                         {count === 0 ? (<div className="text-center px-6 py-10 border-dashed border rounded-2xl border-[#94a3b8ab] text-[#94a3b8ea]"> <h3>Your stack is empty.</h3> </div>)
                             : (<div className="w-full grid gap-2"> <SelectedCard useTecData={useTecData} setUseTecData={setUseTecData} count={count} setCount={setCount} /> </div>)}
 
-                    </div>
-                    {count === 0 ? ('') : (<button onClick={() => {setUseTecData([]);setCount(0);}}
+                        {count === 0 ? ('') : (<button onClick={() => { setUseTecData([]); setCount(0); }}
                             className="btn w-full mt-2 py-6 text-red-400 border border-red-400 text-[18px]">
                             Remove All
-                          </button>
-                    )}
+                        </button>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
